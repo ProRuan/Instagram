bb7d8927294f7517a288c036e1ac38df06538df6
Your Name <you@example.com>
2023-12-05 16:25:55 +0100

    added like-button and like-function

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ let posts = [
         'sub': 'Tullnerfeld',
         'img': './img/wheat.jpg',
         'alt': 'wheat',
-        'posts': ['Der Weizen ist das wahre Gold. Das Gold, dass man auch essen kann.']
+        'likes': 239,
+        'like-state': false,
+        'comments': ['Der Weizen ist das wahre Gold. Das Gold, dass man auch essen kann.']
     },
     {
         'logo': 'forest',
@@ -14,7 +16,9 @@ let posts = [
         'sub': 'Wienerwald',
         'img': './img/forest.jpg',
         'alt': 'forest',
-        'posts': ['Ein Spaziergang im Wald verleiht dem Geist neue Kraft und Kreativität.']
+        'likes': 126,
+        'like-state': false,
+        'comments': ['Ein Spaziergang im Wald verleiht dem Geist neue Kraft und Kreativität.']
     },
     {
         'logo': 'locomotive',
@@ -22,7 +26,9 @@ let posts = [
         'sub': 'Österreich',
         'img': './img/locomotive.jpg',
         'alt': 'locomotive',
-        'posts': ['Am 12. Dezember 2023 öffnet das Eisenbahnermuseum eine neue Ausstellung zum Thema Eisenbahn & Nostalgie.']
+        'likes': 54,
+        'like-state': false,
+        'comments': ['Am 12. Dezember 2023 öffnet das Eisenbahnermuseum eine neue Ausstellung zum Thema Eisenbahn & Nostalgie.']
     }
 ]
 
@@ -48,8 +54,9 @@ function fillPostCascade(postCascade) {
         addPostContainer(postCascade, i);
     }
     for (let i = 0; i < posts.length; i++) {    // Bitte ueberdenken!
-        writeAddedPosts(i);
+        writeAddedComments(i);
     }
+    setLikeButtons();
 }
 
 
@@ -58,20 +65,24 @@ function addPostContainer(postCascade, i) {    // Bitte HTML-Code ueberdenken!
         <article id="post-container-${i}" class="post-container">
             <div id="post-header-${i}" class="post-header">
                 <div class="logo-box ${getLogo(i)}"></div>
-                <div>
+                <div class="post-header-text">
                     <h3 id="author-${i}">${getAuthor(i)}</h3>
                     <span id="sub-text-${i}" class="sub-text">${getSub(i)}</span>
                 </div>    
             </div>
             <img id="post-img-${i}" src="${getImg(i)}" alt="${getAlt(i)}" class="post-img">
-            <p id="post-collector-${i}" class="post-collector">
-                <span id="post-text-${i}" class="post-text">
-                    <b id="post-author-${i}">${getAuthor(i)}</b>
-                    ${getFirstPost(i)}
+            <div id="post-button-bar-${i}" class="post-button-bar">
+                <button id="post-like-button-${i}" class="post-like-button" onclick="likePost(${i})">Like</button>
+            </div>
+            <span id="post-likes-${i}" class="post-likes">${getLikes(i)} Likes</span>
+            <p id="comment-collector-${i}" class="comment-collector">
+                <span id="comment-text-${i}" class="comment-text">
+                    <b id="comment-author-${i}">${getAuthor(i)}</b>
+                    ${getFirstComment(i)}
                 </span>
             </p>
-            <form id="post-form-${i}" onsubmit="addPost(${i})">
-                <input id="input-post-${i}" class="input-post post-text" type="text" placeholder="Post hinzufügen" required>
+            <form id="comment-form-${i}" onsubmit="addComment(${i})">
+                <input id="input-comment-${i}" class="input-comment comment-text" type="text" placeholder="Kommentar hinzufügen" required>
             </form>
         </article>
     `;
@@ -105,14 +116,21 @@ function getAlt(i) {
 }
 
 
-function getFirstPost(i) {
-    return posts[i]['posts'][0];
+function getLikes(i) {
+    if (posts[i]['likes'] > 0) {
+        return posts[i]['likes'];
+    }
 }
 
 
-function addPost(i) {
-    let post = document.getElementById(`input-post-${i}`).value;
-    posts[i]['posts'].push(post);
+function getFirstComment(i) {
+    return posts[i]['comments'][0];
+}
+
+
+function addComment(i) {
+    let comment = document.getElementById(`input-comment-${i}`).value;
+    posts[i]['comments'].push(comment);
     saveAndShowPosts();
 }
 
@@ -136,20 +154,62 @@ function load() {
 }
 
 
-function getAddedPosts(i) {
-    return posts[i]['posts'];
+function likePost(i) {
+    if (posts[i]['like-state'] == false) {
+        addLike(i);
+    } else {
+        removeLike(i);
+    }
+}
+
+function addLike(i) {
+    let newLikes = ++posts[i]['likes'];
+    let likes = document.getElementById(`post-likes-${i}`);
+    likes.innerHTML = `${newLikes} Likes`;
+    posts[i]['like-state'] = true;
+    document.getElementById(`post-like-button-${i}`).classList.add('post-like-button-used');
+    save();
 }
 
 
-function writeAddedPosts(i) {
-    let addedPosts = getAddedPosts(i);
-    for (let j = 1; j < addedPosts.length; j++) {
-        let postText = document.getElementById(`post-collector-${i}`);
-        postText.innerHTML += `
-        <span class="post-text">
+function removeLike(i) {
+    let newLikes = --posts[i]['likes'];
+    let likes = document.getElementById(`post-likes-${i}`);
+    likes.innerHTML = `${newLikes} Likes`;
+    posts[i]['like-state'] = false;
+    document.getElementById(`post-like-button-${i}`).classList.remove('post-like-button-used');
+    save();
+}
+
+
+function setLikeButtons() {
+    for (let i = 0; i < posts.length; i++) {
+        if (posts[i]['like-state'] == true) {
+            document.getElementById(`post-like-button-${i}`).classList.add('post-like-button-used');
+        } else {
+            document.getElementById(`post-like-button-${i}`).classList.remove('post-like-button-used');
+        }
+    }
+}
+
+
+function getAddedComments(i) {
+    return posts[i]['comments'];
+}
+
+
+function writeAddedComments(i) {
+    let addedComments = getAddedComments(i);
+    for (let j = 1; j < addedComments.length; j++) {
+        let commentText = document.getElementById(`comment-collector-${i}`);
+        commentText.innerHTML += `
+        <span id="comment-text-${j}" class="comment-text">
             <b>ruan</b>
-            ${addedPosts[j]}
+            ${addedComments[j]}
         </span>
         `;
     }
 }
+
+
+// letzte Funktionen verbessern!!!
